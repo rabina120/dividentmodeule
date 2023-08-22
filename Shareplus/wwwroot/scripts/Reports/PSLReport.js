a5f4fbce76df22194f51b9faa30772bec7ebf8c8
stha95 <santoshstha440@gmail.com>
2023-08-22 15:42:51 +0545

    Changes

--- Shareplus/wwwroot/scripts/Reports/PSLReport.js
+++ Shareplus/wwwroot/scripts/Reports/PSLReport.js
@@ -256,11 +256,17 @@ var PSLReport = function () {
                     dataType: "json",
                     success: (result) => {
                         if (result.isSuccess) {
-                            var fileName = result.message;
-                            var a = document.createElement("a");
-                            a.href = "data:application/octet-stream;base64," + result.responseData;
-                            a.download = fileName;
-                            a.click();
+                            var embed = "<embed width='100%' height='100%'  type='application/pdf' src='data:application/pdf;base64," + result.responseData + "'/>"
+                            var x = window.open();
+                            x.document.open();
+                            x.document.write(embed);
+                            x.document.title = result.message;
+
+                            //var fileName = result.message;
+                            //var a = document.createElement("a");
+                            //a.href = "data:application/octet-stream;base64," + result.responseData;
+                            //a.download = fileName;
+                            //a.click();
                         }
                         else {
                             alert('error', result.message);
