92049fd49abf3c0b051adfcc2113f4e23784ed10
stha95 <santoshstha440@gmail.com>
2023-08-23 17:35:54 +0545

    updates

--- Shareplus/wwwroot/scripts/Reports/CertificateSplitReport.js
+++ Shareplus/wwwroot/scripts/Reports/CertificateSplitReport.js
@@ -98,12 +98,16 @@ var CertificateSplitReport = function () {
                     dataType: "json",
                     success: (result) => {
                         if (result.isSuccess) {
-                            alert('success', "Report Generated Successfully");
-                            var fileName = result.message;
-                            var a = document.createElement("a");
-                            a.href = "data:application/octet-stream;base64," + result.responseData;
-                            a.download = fileName;
-                            a.click();
+                            var embed = "<embed width='100%' height='100%'  type='application/pdf' src='data:application/pdf;base64," + result.responseData + "'/>"
+                            var x = window.open();
+                            if (x) {
+                                x.document.open();
+                                x.document.write(embed);
+                                x.document.title = result.message;
+                                x.document.close();
+                            } else {
+                                alert('Failed to open new window.');
+                            }
                         } else {
                             alert('error', result.message)
                         }
