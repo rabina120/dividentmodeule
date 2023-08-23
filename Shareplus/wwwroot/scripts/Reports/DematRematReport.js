4f9d7eee8d6da6a7c0eb0d5afed828cb10452058
stha95 <santoshstha440@gmail.com>
2023-08-23 17:35:54 +0545

    updates

--- Shareplus/wwwroot/scripts/Reports/DematRematReport.js
+++ Shareplus/wwwroot/scripts/Reports/DematRematReport.js
@@ -394,11 +394,34 @@ var DematRematReport = function () {
                 dataType: "json",
                 success: (result) => {
                     if (result.isSuccess) {
-                        var fileName = result.message;
-                        var a = document.createElement("a");
-                        a.href = "data:application/octet-stream;base64," + result.responseData;
-                        a.download = fileName;
-                        a.click();
+                        if (data == 'E') {
+                            var fileName = result.message;
+                            var a = document.createElement("a");
+                            a.href = "data:application/octet-stream;base64," + result.responseData;
+                            a.download = fileName;
+                            a.click();
+                        }
+                        else {
+                            var embed = "<embed width='100%' height='100%'  type='application/pdf' src='data:application/pdf;base64," + result.responseData + "'/>"
+                            var x = window.open();
+                            if (x) {
+                                x.document.open();
+                                x.document.write(embed);
+                                x.document.title = result.message;
+                                x.document.close();
+                            } else {
+                                alert('warning', 'Failed to View Report.');
+                                alert('success', 'Downloading pdf repot');
+                                var fileName = result.message;
+                                var a = document.createElement("a");
+                                a.href = "data:application/octet-stream;base64," + result.responseData;
+                                a.download = fileName;
+                                a.click();
+                            }
+                        }
+
+
+                        
                     } else {
                         alert('error', result.message)
                     }
